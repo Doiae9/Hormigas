f9b4994a697469a7d956a7c38c215cb09cb64c81
Leonardo Garcia <l21121603@morelia.tecnm.mx>
2025-06-15 17:17:46 -0600

    -Quitar los comentarios -Revisar columnas iguales

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
         int [][] C ={{-1,-1,0,0,1},{1,0,0,-1,0},{0,1,-1,0,0},{0,1,0,1,-1},{1,0,1,0,-1}};
-        int [] Sl ={1,0,1,0,1};
-        int [] St ={0,1,0,1,0};
+        int [] Sl ={1,0,0,0,0};
+        int [] St ={0,0,0,0,0};
         Proyector.ImprimirMatiz(C);
         System.out.println("--------------");
         int [][] Cl =Proyector.ProyectarLugares(C,Sl);
@@ -14,10 +14,26 @@ public class Main {
         int [][] Ct =Proyector.ProyectarTransiciones(Cl,St);
         Proyector.ImprimirMatiz(Ct);
         System.out.println("---------------");
-        System.out.println("Funcion para C-");
+        System.out.println("Funcion para C_");
+        //delaramos una variable para la C proyectada
         int [][]C_=Proyector.ProyeccionC(C,Sl,St);
         Proyector.ImprimirMatiz(C_);
         boolean esCero = Proyector.ComprobarCero(C_);
-    }
+        System.out.println(esCero);
 
+        //Comparar Columnas
+        boolean ColumnasIguales = Proyector.ColumnasIguales(C_,0,1);
+        System.out.println(ColumnasIguales);
+
+        boolean Redundancia= Proyector.ComprobarRedundancia(C_);
+        System.out.println(Redundancia);
+        if(Proyector.ED(C_)){
+            System.out.println("El sistema cumple con las condiciones");
+        }else{
+            System.out.println("El sistema NO cumple con las condiciones");
+
+        }
+        System.out.println(Proyector.VerificarED(C,Sl,St)?"El sistema es ED":"El sistema no es ED");
+
+    }
 }
--- src/Proyector.java
+++ src/Proyector.java
@@ -26,6 +26,7 @@ public class Proyector {
                 nuevasTransiciones++;
             }
         }
+
        for (int i=0; i<C.length; i++) {
            int[] nuevaFila = new int[nuevasTransiciones];
            int colIndex = 0;
@@ -43,6 +44,7 @@ public class Proyector {
        return resultado;
 
     }
+
 //Proyeccion de ´c o, quitar lugares y transiciones
     public static int [][] ProyeccionC(int [][]C, int [] Sl, int [] St){
         int [][] lugares=  ProyectarLugares(C,Sl);
@@ -50,39 +52,80 @@ public class Proyector {
         return ultimaProyeccion;
     }
 
+    //Comprobar que ninguna columna tenga 0 unicamente
     public static boolean ComprobarCero(int [][] C_){
         if(C_==null) {
-            System.out.println("No existen proyecciones para mostrar");
+            //System.out.println("No existen proyecciones para mostrar");
             return false;
         }
         //Desglozamos los lugares
         int Lugares = C_.length;
         int Transiciones = C_[0].length;
-
+        //[filas][columnas]
         for (int i=0; i<Transiciones; i++) {
             //Cada vez que cambie de columna el bool cambia
             boolean esCero = true;
             for (int j=0; j<Lugares; j++) {
-                if(C_[i][j]!=0){
+                //Se comprueba que exista por lo menos un valor diferente a 0
+                if(C_[j][i]!=0){
                     esCero = false;
                     break;
                 }
             }
             if(esCero){
-                System.out.println("El sistema no es viable");
+               // System.out.println("El sistema no es viable");
+                return true;
+            }
+        }
+       // System.out.println("El sistema sigue siendo viable");
+        return false;
+    }
+    //Funcion auxiliar para comparar columnas -> Recibe la matriz, Recibe una columna,
+    //Recibe otra columna a comparar
+    public static boolean ColumnasIguales(int [][] C_, int i, int j){
+        int filas = C_.length;
+        for (int k=0; k<filas; k++){
+            //Compara que exista una diferencia
+            if(C_[k][i]!=C_[k][j]) {
+                //System.out.println("Comparando columna..." + i + " con: " + j);
                 return false;
+            }
         }
-
-        }
-        System.out.println("El sistema sigue siendo viable");
         return true;
     }
 
+        //Comprueba la redundancia de las columnas
+        //Recibe la C Proyectada
+    public static boolean ComprobarRedundancia(int [][]C_){
+        //Columnas
+        int Transiciones =C_[0].length;
+        for (int i = 0; i < Transiciones-1; i++) {
+           //Comparamos la siguiente fila
+            for (int j =i + 1; j<Transiciones; j++){
+                if(ColumnasIguales(C_,i,j)){
+                 //Si las columnas son iguales...
+                   return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean ED(int C_[][]){
+       return !ComprobarCero(C_) && !ComprobarRedundancia(C_);
 
+
+    }
+    public static boolean VerificarED(int C[][],int [] Sl, int [] St){
+
+        return  ED(ProyeccionC(C, Sl, St)) ? true : false;
+    }
+
+
+    //Imprimir Matriz
     public static void ImprimirMatiz(int [][] matriz){
         for(int i=0; i<matriz.length; i++){
             for(int j=0; j<matriz[i].length; j++){
-                System.out.print(matriz[i][j]+" ");
+               // System.out.print(matriz[i][j]+"\t");
 
             }
             System.out.println(" ");
